d0c30f44cf6263cbe82694b8f7334c4061b9a9fc
upadhyayshubh <upadhyayshubh@gmail.com>
2021-07-08 17:20:13 +0530

    updating .env file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const dotenv =  require("dotenv");
 const GetFileRouter = require("./routes/UploadFileRouter");
 
 const app = express();
+dotenv.config();
 
 app.use("/api", GetFileRouter);
 
-const server = app.listen(5001, ()=>{
-    console.log("listening....")
+const server = app.listen(process.env.PORT, ()=>{
+    console.log("listening....");
 })
 
 module.exports = server;
