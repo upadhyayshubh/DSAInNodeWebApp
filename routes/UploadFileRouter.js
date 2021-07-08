7dc2c9f2a1a870f9a1c2540920e37672f11517ec
upadhyayshubh <upadhyayshubh@gmail.com>
2021-07-08 16:50:13 +0530

    modified upload file router and added post call

--- routes/UploadFileRouter.js
+++ routes/UploadFileRouter.js
@@ -9,4 +9,14 @@ Router.get("/getfile", async (req, res)=>{
         res.send("wrong file");
     }
 });
+
+Router.post("/getfile", async (req, res)=>{
+    try{
+        res.send("correct file");
+    }catch(e){
+        console.log(e);
+        res.send("wrong file");
+    }
+});
+
 module.exports= Router;
