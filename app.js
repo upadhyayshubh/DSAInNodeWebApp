0814f47f93ae0127c580b079f4e4beafc1c0dabf
upadhyayshubh <upadhyayshubh@gmail.com>
2021-08-06 18:52:24 +0530

    Account Balance Type Assignment

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv =  require("dotenv");
 const GetFileRouter = require("./routes/UploadFileRouter");
+const {testAccountBalanceType} = require("./controller/JS/AccountBalanceType");
 
 const app = express();
 dotenv.config();
 
 app.use("/api", GetFileRouter);
+
+testAccountBalanceType();
 
 const server = app.listen(process.env.PORT, ()=>{
     console.log("listening....");
